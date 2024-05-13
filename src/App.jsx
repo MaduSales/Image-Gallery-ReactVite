95abd3fa77d6d4671243c5a86536d9ce39fb0ba2
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-12 21:07:13 -0300

    repositorio quatro

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GaleriaDeFotos from './components/Galeria/Galeria'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <GaleriaDeFotos/>
+    </div>
+  )
+}
